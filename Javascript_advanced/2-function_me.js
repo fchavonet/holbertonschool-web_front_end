8505f43cd5854ec468f0d7fe383b0f1dc5453938
fchavonet <fchavonet@mailo.com>
2024-09-20 09:31:01 +0200

    Uploading the task `2. Closure`.

---
+++ Javascript_advanced/2-function_me.js
@@ -0,0 +1,9 @@
+function welcomeMessage(fullName) {
+	return function () {
+		alert("Welcome" + " " + fullName);
+	};
+}
+
+let guillaume = welcomeMessage("Guillaume");
+let alex = welcomeMessage("Alex");
+let fred = welcomeMessage("Fred");
