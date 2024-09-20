f37473c873c4007177f815c1d78eab6889fd3b93
fchavonet <fchavonet@mailo.com>
2024-09-20 13:58:48 +0200

    Uploading the task `10. Execution stack & timing execution`.

---
+++ Javascript_advanced/10-prime.js
@@ -0,0 +1,37 @@
+function countPrimeNumbers() {
+    let count = 0;
+
+    for (let i = 2; i <= 100; i++) {
+        let isPrime = true;
+
+        for (let j = 2; j <= Math.sqrt(i); j++) {
+            if (i % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) {
+            count++;
+        }
+    }
+    return count;
+}
+
+function executeMultipleTimes(times) {
+    let primeNumbers;
+    let startTime = performance.now();
+
+    for (let i = 0; i < times; i++) {
+        primeNumbers = countPrimeNumbers();
+    }
+
+    let endTime = performance.now();
+
+    let totalTime = (endTime - startTime);
+
+    console.log("There are " + primeNumbers + " prime numbers from 2 to 100.");
+    console.log("They will be calculated " + times + " times.")
+    console.log("Execution time of calculating prime numbers " + times + " times was " + totalTime + " milliseconds.");
+}
+
+executeMultipleTimes(100);
